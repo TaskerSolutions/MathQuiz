880e3f3e61a2940e45e3deedf959f804ea238a14
TaskerSolutions <79877715+TaskerSolutions@users.noreply.github.com>
2021-03-12 17:49:20 +1300

    Initial commit

---
+++ app/src/main/java/com/taskersolutions/mathquiz/MainActivity.java
@@ -0,0 +1,135 @@
+package com.taskersolutions.mathquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn_start, btn_answer0, btn_answer1, btn_answer2, btn_answer3;
+    TextView tv_score, tv_question, tv_timer, tv_message;
+    ProgressBar progress_timer;
+
+
+    Game g = new Game();
+
+    int secondsRemaining = 30;
+
+    CountDownTimer timer = new CountDownTimer(30000, 1000) {
+        @Override
+        public void onTick(long millisUntilFinished) {
+            secondsRemaining--;
+            tv_timer.setText(Integer.toString(secondsRemaining)  + " seconds");
+            progress_timer.setProgress(30 - secondsRemaining);
+        }
+
+        @Override
+        public void onFinish() {
+            btn_answer0.setEnabled(false);
+            btn_answer1.setEnabled(false);
+            btn_answer2.setEnabled(false);
+            btn_answer3.setEnabled(false);
+
+            tv_message.setText("Time is up! " + g.getNumberCorrect() + "/" + (g.getTotalQuestions() - 1));
+
+            final Handler handler = new Handler();
+
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    btn_start.setVisibility(View.VISIBLE);
+                }
+            }, 4000);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_start = findViewById(R.id.btn_start);
+        btn_answer0 = findViewById(R.id.btn_answer0);
+        btn_answer1 = findViewById(R.id.btn_answer1);
+        btn_answer2 = findViewById(R.id.btn_answer2);
+        btn_answer3 = findViewById(R.id.btn_answer3);
+
+        tv_score = findViewById(R.id.tv_score);
+        tv_question = findViewById(R.id.tv_question);
+        tv_timer = findViewById(R.id.tv_timer);
+        tv_message = findViewById(R.id.tv_message);
+
+        progress_timer = findViewById(R.id.prog_timer);
+
+        tv_timer.setText("0Sec");
+        tv_question.setText("");
+        tv_message.setText("Press Start");
+        tv_score.setText("0Pts");
+
+        progress_timer.setProgress(0);
+
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Tells compiler that View v is to come from a button (casting button)
+                Button start_button = (Button) v;
+                start_button.setVisibility(View.INVISIBLE);
+                secondsRemaining = 30;
+                g = new Game();
+                nextTurn();
+                timer.start();
+            }
+        });
+
+        View.OnClickListener answerButtonClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Button buttonClicked = (Button) v;
+
+                int answerSelected = Integer.parseInt(buttonClicked.getText().toString());
+                g.checkAnswer(answerSelected);
+                tv_score.setText(Integer.toString(g.getScore()) + " points");
+
+                nextTurn();
+            }
+        };
+
+        btn_answer0.setOnClickListener(answerButtonClickListener);
+        btn_answer1.setOnClickListener(answerButtonClickListener);
+        btn_answer2.setOnClickListener(answerButtonClickListener);
+        btn_answer3.setOnClickListener(answerButtonClickListener);
+
+    }
+
+    private void nextTurn() {
+
+        g.makeNewQuestion();
+        int [] answer = g.getCurrentQuestion().getAnswerArray();
+
+        // set text on answer buttons
+        btn_answer0.setText(Integer.toString(answer[0]));
+        btn_answer1.setText(Integer.toString(answer[1]));
+        btn_answer2.setText(Integer.toString(answer[2]));
+        btn_answer3.setText(Integer.toString(answer[3]));
+
+        //enable answer buttons
+        btn_answer0.setEnabled(true);
+        btn_answer1.setEnabled(true);
+        btn_answer2.setEnabled(true);
+        btn_answer3.setEnabled(true);
+
+        tv_question.setText(g.getCurrentQuestion().getQuestionPhrase());
+
+        tv_message.setText(g.getNumberCorrect() + "/" + (g.getTotalQuestions() - 1));
+        //start timer
+
+    }
+
+}
